b06db0acb2018e746586d9ed09a9f4d3d5b88a01
Dexter Castor Döpping <dexter.c.dopping@gmail.com>
2021-10-09 00:49:36 +0200

    Add script for building git repo from downloaded files

---
+++ build_linear_repo.py
@@ -0,0 +1,114 @@
+import os
+import subprocess
+from pathlib import Path
+import shutil
+import datetime
+from zoneinfo import ZoneInfo
+import textwrap
+
+import manifest
+
+from settings import decprg_path
+
+
+unsorted_manifests = manifest.load_file('manifests.txt')
+manifests = sorted(unsorted_manifests, key=lambda m: m.datetime)
+
+project = Path('.').resolve()
+repository = project / 'linear_repo'
+
+repository.mkdir(exist_ok=True)
+os.chdir(repository)
+
+has_git = (repository / '.git').exists()
+if not has_git:
+    subprocess.run([
+        'git', 'init',
+            '--initial-branch', 'main'],
+        check=True)
+
+keep_paths = ['.git']
+def clear_repo():
+    for item in repository.iterdir():
+        if item.name in keep_paths:
+            continue
+
+        if item.is_file():
+            item.unlink()
+        else:
+            shutil.rmtree(item)
+
+
+for manifest in manifests:
+    manifest_location = project / 'downloads' / str(manifest.code)
+    assert manifest_location.exists()
+
+    print('Processing path', manifest_location)
+
+    clear_repo()
+    shutil.copytree(manifest_location, repository, dirs_exist_ok=True)
+
+    dectypes = ['v1', 'v2', 'plain']
+    decsuccess = False
+
+    wak = repository / 'data/data.wak'
+    for dectype in dectypes:
+        try:
+            subprocess.run([
+                decprg_path, dectype,
+                    wak,
+                    repository],
+                check=True)
+            decsuccess = True
+        except:
+            pass
+
+    if not decsuccess:
+        raise Exception('Could not decrypt')
+
+    wak.unlink()
+
+    print('Converting text files to unix format')
+    subprocess.run([
+        'find', '.', '-type', 'f',
+            '!', '-path', './.git/*',
+            '-regextype', 'egrep',
+            '-regex', '.*\.(txt|lua|xml|csv|bat|frag|vert)',
+            '-exec', 'dos2unix', '{}', '+'],
+        check=True)
+
+    print('Removing trailing spaces')
+    subprocess.run([
+        'find', '.', '-type', 'f',
+            '!', '-path', './.git/*',
+            '-regextype', 'egrep',
+            '-regex', '.*\.(txt|lua|xml|csv|bat|frag|vert)',
+            '-exec', 'sed', '-i', 's/\s\+$//', '{}', '+'],
+        check=True)
+
+    dt = manifest.datetime.astimezone(ZoneInfo('Europe/Helsinki'))
+    commit_message = textwrap.dedent(f"""\
+    Commit manifest {manifest.code}
+
+    Published on {dt.isoformat()}
+    """)
+
+    branch = repository / '_branch.txt'
+    if branch.exists():
+        commit_message += '\n\n' + textwrap.dedent(f"""\
+        Branch: {open(branch).read()}
+        """)
+
+    version_hash = repository / '_version_hash.txt'
+    if version_hash.exists():
+        commit_message += '\n\n' + textwrap.dedent(f"""\
+        Version Hash: {open(version_hash).read()}
+        """)
+
+    subprocess.run(['git', 'add', '.'], check=True)
+    subprocess.run([
+        'git', 'commit',
+            '-m', commit_message,
+            '--author=Nolla Games <noita@nollagames.com>',
+            '--date', dt.isoformat()],
+        check=True)
